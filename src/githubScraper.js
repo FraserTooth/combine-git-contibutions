3e92144ee66583d1c1388bcf1c9d8c597f005475
Fraser Tooth <frasertooth@gmail.com>
2020-04-12 20:50:33 +0900

    Date parsing added, package.json set up

--- src/githubScraper.js
+++ src/githubScraper.js
@@ -9,7 +9,7 @@ const getContributions = async (githubURL) => {
   const days = $(".day");
 
   days.each((i, element) => {
-    const date = element.attribs["data-date"];
+    const date = Date.parse(element.attribs["data-date"]);
     const count = element.attribs["data-count"];
     if (!date) return;
     data.push({
