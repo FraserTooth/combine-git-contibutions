470a164d6fafeb39c7a902173206f15cc56a9db9
Fraser Tooth <frasertooth@gmail.com>
2020-05-05 11:21:31 +0900

    Reworked tests and added non-array use

--- src/combineGitContributions.js
+++ src/combineGitContributions.js
@@ -2,16 +2,26 @@ const githubScraper = require("./githubScraper");
 
 const getAllContributions = async (urlsArray) => {
   if (urlsArray === undefined) return undefined;
-  const results = urlsArray.map(async (url) => {
+  if (!Array.isArray(urlsArray)) {
+    const url = urlsArray;
     if (url.match("github.com")) {
       return await githubScraper.getContributions(url);
-    } else if (url.match("gitlab.com")) {
-      return; //Will write the gitlab integration at some point
     }
-  });
-  return Promise.all(results).then((results) => {
-    return results;
-  });
+  }
+  try {
+    const results = urlsArray.map(async (url) => {
+      if (url.match("github.com")) {
+        return await githubScraper.getContributions(url);
+      } else if (url.match("gitlab.com")) {
+        return; //Will write the gitlab integration at some point
+      }
+    });
+    return Promise.all(results).then((results) => {
+      return results;
+    });
+  } catch (error) {
+    throw Error(error);
+  }
 };
 
 module.exports = getAllContributions;
--- test/main_test.js
+++ test/main_test.js
@@ -1,8 +1,11 @@
 const contributions = require("../src/combineGitContributions");
 const expect = require("chai").expect;
 
+const testURL1 = "https://www.github.com/FraserTooth";
+const testURL2 = "https://www.github.com/mt-ftooth";
+
 describe("Library Test", function () {
-  this.timeout(5000);
+  this.timeout(10000);
   context("with no Arguments", () => {
     it("should return undefined", () => {
       contributions().then((result) => {
@@ -20,17 +23,37 @@ describe("Library Test", function () {
       });
     });
     context("using one github link", () => {
-      const testURL = "https://www.github.com/FraserTooth";
-
-      it("should return an array of contributions data", (done) => {
-        const output = contributions([testURL]);
+      it("should return an array of arrays of contributions data", (done) => {
+        const output = contributions([testURL1]);
         output.then((output) => {
           expect(output[0].length).to.be.greaterThan(1);
+          expect(output.length).to.equal(1);
+          done();
+        });
+      });
+    });
+    context("using two github links", () => {
+      it("should return an array of arrays of contributions data", (done) => {
+        const output = contributions([testURL1, testURL2]);
+        output.then((output) => {
+          expect(output[0].length).to.be.greaterThan(1);
+          expect(output[1].length).to.be.greaterThan(1);
+          expect(output.length).to.equal(2);
           done();
         });
       });
     });
   });
 
-  context("with non array arguments", () => {});
+  context("with non array arguments", () => {
+    context("using one github link", () => {
+      it("should return an array of contributions data", (done) => {
+        const output = contributions(testURL1);
+        output.then((output) => {
+          expect(output.length).to.be.greaterThan(1);
+          done();
+        });
+      });
+    });
+  });
 });
