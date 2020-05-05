3c69df4a9a26a505a278a30b11a4e3665aa7dfb7
Fraser Tooth <frasertooth@gmail.com>
2020-05-05 17:22:29 +0900

    Host of changes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # combine-git-contibutions
+
 Gather data about git contributions from mutliple websites and profiles.
+
+## Using this Repo
+
+Install all the dependancies using yarn:
+
+```console
+    yarn
+```
+
+If you want to run tests:
+
+```console
+    yarn test
+```
+
+If you make a change to the example site, rebuild the bundle file with:
+
+```console
+    yarn build
+```
--- example_site/index.js
+++ example_site/index.js
@@ -1,7 +1,7 @@
 import contributions from "../src/combineGitContributions";
 import "bootstrap";
-
-console.log("Hello");
+import _ from "lodash";
+import moment from "moment";
 
 const getContributions = async () => {
   const githubUrl1 = "https://github.com/FraserTooth";
@@ -13,7 +13,37 @@ const getContributions = async () => {
   const url2 = corsURL + githubUrl2;
 
   const allContributions = await contributions([url1, url2]);
-  console.log(allContributions);
+
+  //Combine the histories
+  const combined = [];
+  for (let i = 0; i < allContributions[0].length; i++) {
+    const repo1Element = allContributions[0][i];
+    const repo2Element = allContributions[1][i];
+
+    const date = new Date(repo1Element.date);
+
+    combined.push({
+      date,
+      count: parseInt(repo1Element.count) + parseInt(repo2Element.count),
+      dayOfWeek: date.getDay(),
+    });
+  }
+  console.log(combined);
+
+  //Build a Github-esque graph
+  const graph = document.getElementById("graph");
+
+  combined.forEach((item) => {
+    //Create Graph Element
+    const graphElement = document.createElement("div");
+    graphElement.dataset.date = item.date;
+    graphElement.dataset.count = item.count;
+    graphElement.style.color = "green";
+    graphElement.className = "graphElement";
+
+    //Sort Into Rows
+    graphChildren[item.dayOfWeek].appendChild(graphElement);
+  });
 };
 
 getContributions();
