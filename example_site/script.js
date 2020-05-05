40dcda3673e4d1b4f9a0d74bf54eb9ef8afa5584
Fraser Tooth <frasertooth@gmail.com>
2020-05-05 12:42:35 +0900

    Added webpack

--- example_site/script.js
+++ example_site/script.js
@@ -1,1 +1,5 @@
-console.log("Hello");
+const githubUrl1 = "https://github.com/FraserTooth";
+const githubUrl2 = "https://github.com/mt-ftooth";
+const allContributions = contributions([githubUrl1, githubUrl2]);
+
+console.log(allContributions);
